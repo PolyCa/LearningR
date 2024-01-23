3a8f842f1a3c0def8e6510b367edc205a43eb946
Christian Velling Thorup <christian2rup@hotmail.com>
2024-01-23 16:28:41 +0100

    added r/learning.R

--- R/learning.R
+++ R/learning.R
@@ -2,3 +2,10 @@
 # Loading packages --------------------------------------------------------
 
 library(tidyverse)
+#This will be used for testing out Git
+
+install.packages("usethis")
+# install.packages("pak")
+pak::pak("r-lib/usethis")
+usethis::create_github_token()
+
