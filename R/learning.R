c8f9f8d3b012c7272af51243bf7869a84faf4cf7
Christian Velling Thorup <christian2rup@hotmail.com>
2024-01-24 11:28:51 +0100

    NHANES wrangling opg 7

--- R/learning.R
+++ R/learning.R
@@ -1,11 +1,62 @@
-
-# Loading packages --------------------------------------------------------
+# Loading Packages
 
 library(tidyverse)
-#This will be used for testing out Git
+library(NHANES)
 
-install.packages("usethis")
-# install.packages("pak")
-pak::pak("r-lib/usethis")
-usethis::create_github_token()
+glimpse(NHANES)
+str(NHANES)
 
+# Select specific colomuns ------------------------------------------------
+
+select(NHANES, Age, Weight, BMI)
+
+select(NHANES, -HeadCirc)
+
+select(NHANES, starts_with("BP"))
+
+select(NHANES, ends_with("Day"))
+
+select(NHANES, contains("Age"))
+
+# Rename all columns ------------------------------------------------------
+
+rename_with(NHANES, snakecase::to_snake_case)
+
+NHANES_small <- rename_with(NHANES, snakecase::to_snake_case)
+
+
+# Rename specific column --------------------------------------------------
+
+NHANES_small <- rename(NHANES_small, sex = gender)
+
+NHANES_small
+
+View(NHANES_small)
+
+# Chaining the function with pipe -----------------------------------------
+
+colnames(NHANES_small)
+
+NHANES_small %>%
+  colnames()
+
+NHANES_phys <- NHANES_small %>%
+  select(phys_active) %>%
+  rename(physically_active = phys_active)
+
+NHANES_small %>%
+  select(bp_sys_ave, education)
+
+NHANES_small %>%
+  rename(
+    bp_sys = bp_sys_ave,
+    bp_dia = bp_dia_ave
+  )
+
+NHANES_small %>%
+  select(bmi) %>%
+  select(contains("age"))
+
+NHANES_small %>%
+  select(starts_with("bp_")) %>%
+  rename(bp_systolic = bp_sys_ave)
